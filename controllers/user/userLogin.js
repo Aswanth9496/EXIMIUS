00154bb40e17143a5173bc3e3dcef2c7912594d9
Aswanth9496 <aswanthgovindan@gmail.com>
2024-10-15 09:59:45 +0530

    last commit

--- controllers/user/userLogin.js
+++ controllers/user/userLogin.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK,
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Get email from the profile
